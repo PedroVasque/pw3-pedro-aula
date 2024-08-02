d7f17bc4ef6a75a1019ec41f4a74f8ddec49e03b
PedroVasque <pr529017@gmail.com>
2024-08-01 22:21:25 -0300

    Limpeza dos arquivos CSS e criação dos primeiros components

---
+++ projetopw3/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import CardBooks from './components/CardBooks'
+function App() {
+
+  return (
+    <>
+      
+      <CardBooks 
+        titulo = '50 Tons de Preto'
+        autor = 'Kid Bengala'
+      />
+      <CardBooks 
+        titulo = '50 Tons de Branco'
+        autor = 'Gabriel Prudencio'
+      />
+      <CardBooks 
+        titulo = '50 Tons de Pardo'
+        autor = 'Pedro Ribeiro'
+      />
+
+    
+      
+    </>
+  )
+}
+
+export default App
